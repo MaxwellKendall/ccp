8d80daca7c9ca3446c8cb8a072164c7990328acc
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2019-12-25 09:37:17 -0500

    sourcing from wp

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,35 @@
  */
 
 // You can delete this file if you're not using it
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allWordpressPost(sort: { fields: [date] }) {
+        edges {
+          node {
+            title
+            excerpt
+            content
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    console.log(JSON.stringify(result, null, 4))
+    result.data.allWordpressPost.edges.forEach(({ node }) => {
+        createPage({
+          path: node.slug,
+          component: path.resolve(`./src/templates/blog-post.js`),
+          context: {
+            // This is the $slug variable
+            // passed to blog-post.js
+            slug: node.slug,
+          },
+        })
+      });
+  });
+};
