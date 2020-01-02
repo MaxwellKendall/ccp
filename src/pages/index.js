8b5ab910ca455e3efe7eb41965cbb068a6a8dae7
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-01-01 23:36:30 -0500

    using page content for carosel

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 export default ({ data }) => {
+  const { wordpressPage } = data;
   return (
     <Layout>
         <SEO title="home" />
         <h1>CCP v2</h1>
+        <div dangerouslySetInnerHTML={{ __html: wordpressPage.content }} />
         <Link to={`blog`}>Go to the Blog</Link>
         <Link to={`sermons`}>Go to the Sermon Archive</Link>
     </Layout>
@@ -24,6 +26,9 @@ export const pageQuery = graphql`
           slug
         }
       }
+    }
+    wordpressPage(path: {eq: "/"}) {
+      content
     }
   }
 `
