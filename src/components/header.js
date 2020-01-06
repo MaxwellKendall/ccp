5a69424df48c4bdce815ca26eda39b55348bd164
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-01-05 21:47:36 -0500

    aligns header, adds search in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,6 @@
 import { Link, useStaticQuery } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
 import Img from "gatsby-image"
 import PropTypes from "prop-types"
 import React from "react"
@@ -8,7 +10,7 @@ const Header = ({ siteTitle }) => {
     query {
       file(relativePath: {eq: "ccp_logo.png"}) {
         childImageSharp {
-          resize(height: 144) {
+          resize(height: 30, width: 150) {
             src
             tracedSVG
             width
@@ -20,11 +22,11 @@ const Header = ({ siteTitle }) => {
     }
   `)
   return (
-    <header className="bg-black">
-      <Link to="/">
-        <Img fixed={data.file.childImageSharp.resize} />
-        <h1>{siteTitle}</h1>
+    <header className="bg-black h-8 flex items-center justify-start">
+      <Link className="ml-2" to="/">
+        <Img className="align-middle" fixed={data.file.childImageSharp.resize} />
       </Link>
+      <FontAwesomeIcon className="text-white ml-auto mr-5" icon={faSearch} />
     </header>
   );
 }
