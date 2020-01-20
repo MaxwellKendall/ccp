a5a64475cd574f308a92e23d9d3808731aaaa585
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-01-20 14:06:25 -0500

    removing unused import

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react"
+import React, { useState } from "react"
 import { Link, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 import PropTypes from "prop-types"
