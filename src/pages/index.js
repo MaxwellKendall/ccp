dac2376b52f951f6569d0b2c6fbccafdf71be06c
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-01-29 16:29:58 -0500

    adding sub nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
 import moment from "moment"
+import { camelCase } from "lodash"
+import cx from "classnames"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,52 +10,73 @@ import Carousel from "../components/carousel"
 import Card from "../components/card"
 import { SermonExcerpt } from "./sermons"
 
+const subMenu = ["Upcoming Events", "Recent Sermons", "Recent Articles"]
+
+const filterEvents = (event) => {
+  return moment(event.start.dateTime).isAfter(moment())
+}
+
+const ActiveSection = ({ section, data }) => {
+  console.log(section, data, "******")
+  if (section === "upcomingEvents") {
+    return data.upcomingEvents.edges
+      .filter(({ node }) => filterEvents(node))
+      .slice(0, 5)
+      .map(({ node }) => (
+          <Card slug={`events/${node.slug}`} title={node.summary} key={node.id}>
+            <>
+              <p>{node.description}</p>
+              <p>{node.location}</p>
+              {node.attachments && (
+                node.attachments.map((attachment) => {
+                  return <a href={attachment.fileUrl}>{attachment.title}</a>
+                })
+              )}
+            </>
+          </Card>
+    ))
+  }
+  else if (section === "recentSermons") {
+    return data.recentSermons.edges
+      .map(({ node }) => {
+        console.log("sermon", node)
+        return (
+          <Card slug={`sermons/${node.slug}`} key={node.id} title={node.fullTitle}>
+            <SermonExcerpt sermon={node} />
+          </Card>
+        )
+      })
+  }
+  else if (section === "recentArticles") {
+    return data.recentBlogPosts.edges
+      .map(({ node }) => (
+        <Card slug={`blog/${node.slug}`} title={node.title}>
+          <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+        </Card>
+      ))
+  }
+  return null
+}
+
 export default ({ data }) => {
-  const { images, recentSermons, upcomingEvents, recentBlogPosts } = data
+  const [activeTab, setActiveTab] = useState(camelCase(subMenu[0]))
+
   return (
     <Layout>
       <SEO title="Home" />
       <Carousel
         data={images}
         totalSlides={images.edges.length} />
-        <h2>Upcoming Events</h2>
-        {upcomingEvents.edges
-          .filter(({ node }) => {
-            return (moment(node.start.dateTime).isAfter(moment()))
-          })
-          .slice(0, 5)
-          .map(({ node }) => (
-              <Card slug={`events/${node.slug}`} title={node.summary} key={node.id}>
-                <>
-                  <p>{node.description}</p>
-                  <p>{node.location}</p>
-                  {node.attachments && (
-                    node.attachments.map((attachment) => {
-                      return <a href={attachment.fileUrl}>{attachment.title}</a>
-                    })
-                  )}
-                </>
-              </Card>
-        ))}
-        <h2>Recent Sermons</h2>
-        {recentSermons.edges
-          // .map(({ node }) => console.log("sermon", sermon) true)
-          .map(({ node }) => {
-            console.log("sermon", node)
-            return (
-              <Card slug={`sermons/${node.slug}`} key={node.id} title={node.fullTitle}>
-                <SermonExcerpt sermon={node} />
-              </Card>
-            )
-          })}
-        <h2>Recent Articles</h2>
-        {recentBlogPosts.edges
-          .map(({ node }) => (
-            <Card slug={`blog/${node.slug}`} title={node.title}>
-              <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </Card>
-          ))
-        }
+        <ul className="m-0 flex items-center home-page__subnav">
+          {subMenu.map((item, i) => (
+            <li
+              className={`px-2 py-6 m-0 w-full ${cx({ active: activeTab === camelCase(item)})}`}
+              onClick={setActiveTab.bind(null, camelCase(item))}
+              key={i}>
+                {item}
+            </li>))}
+        </ul>
+        <ActiveSection section={activeTab} data={data} />
     </Layout>
   )
 }
