dd374abcd29f2c32a18d9fc4a74c97484d881054
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-03-25 14:16:59 -0400

    sub nav polish

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,7 +68,7 @@ export default ({ data }) => {
         <ul className="m-0 flex items-center justify-center home-page__subnav">
           {subMenu.map((item, i) => (
             <li
-              className={`rounded text-center font-semibold px-8 py-4 h-16 m-0 ${cx({ 'active': activeTab === camelCase(item)})}`}
+              className={`rounded px-4 py-2 text-center font-semibold md:px-8 md:py-4 md:h-16 m-0 ${cx({ 'active': activeTab === camelCase(item)})}`}
               onClick={setActiveTab.bind(null, camelCase(item))}
               key={i}>
                 {item}
