9418d3ad5a288916f877bb0190b19a1cc7e8dacb
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-03-28 11:42:39 -0400

    mobile issue w/ subnav

--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 const Card = ({ title, children, slug }) => {
   return (
-    <Link className="md:w-1/2 mx-auto no-underline" to={`/${slug}`}>
+    <Link className="w-full md:w-5/6 max-w-4xl mx-4 no-underline" to={`/${slug}`}>
       <div className="mb-5 mx-auto w-full bg-white hover:bg-white shadow-lg rounded px-5 py-10">
         {slug.includes('blog') && <h2 dangerouslySetInnerHTML={{ __html: title }}/>}
         {!slug.includes('blog') && <h2>{title}</h2>}
